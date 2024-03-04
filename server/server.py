0248803939d8abea05bf44c52b9942a8fc6e283f
Rahil Shah <rshah10@bu.edu>
2024-03-03 22:00:14 -0500

    added a temporary home page and started working on database

--- server/server.py
+++ server/server.py
@@ -1,36 +1,56 @@
 from flask import Flask, url_for, session
 from flask import render_template, redirect
-from authlib.integrations.flask_client import OAuth
-import sqlalchemy, os, requests
+from flask_sqlalchemy import SQLAlchemy
+#from authlib.integrations.flask_client import OAuth
+from sqlalchemy.orm import DeclarativeBase
+import os, requests
 
-app = Flask(__name__)
-oauth = OAuth(app)
+class Base(DeclarativeBase):
+  pass
+
+app = Flask(__name__, template_folder='../client/templates')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+db = SQLAlchemy(app)
+#oauth = OAuth(app)
 
 GOOGLE_CLIENT_ID = os.getenv('CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
-CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
-oauth.register(
-    name='google',
-    server_metadata_url=CONF_URL,
-    client_kwargs={
-        'scope': 'openid email profile'
-    }
-)
+# CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+# oauth.register(
+#     name='google',
+#     server_metadata_url=CONF_URL,
+#     client_kwargs={
+#         'scope': 'openid email profile'
+#     }
+# )
+
+# creating database models
+# class User(db.Model):
+#     pass
+# class Itinerary(db.Model):
+#     pass
+
+# all the routes
+# @app.route("/users")
+# def user_list():
+#     users = db.session.execute(db.select(User).order_by(User.username)).scalars()
+#     return render_template("user/list.html", users=users)
 
 @app.route('/')
 def homepage():
-    user = session.get('user')
-    return render_template('home.html', user=user)
+    #user = session.get('user')
+    return render_template('home.html')
 
 @app.route('/login')
 def login():
     redirect_uri = url_for('auth', _external=True)
-    return oauth.google.authorize_redirect(redirect_uri)
+    #return oauth.google.authorize_redirect(redirect_uri)
+    pass
 
 @app.route('/auth')
 def auth():
-    token = oauth.google.authorize_access_token()
+    #token = oauth.google.authorize_access_token()
     session['user'] = token['userinfo']
     return redirect('/')
 
